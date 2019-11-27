05efc3f076263bc1bc4e835c19a4d24c8af7c1ca
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-27 18:59:56 +0600

    frontend slider image show

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
+
     public function index()
     {
-        return view('backend.dashboard');
+        $sliders = Slider::all();
+        return view('welcome', compact('sliders'));
     }
 }
