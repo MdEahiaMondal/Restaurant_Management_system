47398a710fd1c3e91ebc37b52b270bab97284d36
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-27 08:46:14 +0600

    backend mastaring complete

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::get('/dashboard', function ()
+{
+    return view('backend.master.master');
+});
