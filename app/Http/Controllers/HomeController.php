c1921dfe908f227676a728f0ee7daf1879ebc0ff
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-28 11:51:53 +0600

    item and category show on frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Item;
 use App\Slider;
 use Illuminate\Http\Request;
 
@@ -13,6 +15,8 @@ class HomeController extends Controller
     public function index()
     {
         $sliders = Slider::all();
-        return view('welcome', compact('sliders'));
+        $items = Item::all();
+        $categories = Category::all();
+        return view('welcome', compact('sliders','items', 'categories'));
     }
 }
